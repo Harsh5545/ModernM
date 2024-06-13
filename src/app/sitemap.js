ae4eff3dff891f3091683c65dba2751f1ea65897
harshad <Harshadkajale333@gmail.com>
2024-06-13 23:39:30 +0530

    almost

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,17 +1,13 @@
-import { getPosts } from "@/lib/data";
+
 
 const WEBSITE_HOST_URL = 'https://modernmannerism.com';
 
 export default async function sitemap() {
-    const articles = await getPosts();
+ 
     
     const changeFrequency = 'daily';
 
-    const posts = articles.map(({ slug, createdAt }) => ({
-        url: `${WEBSITE_HOST_URL}/blog/${slug}`,
-        lastModified: createdAt.toISOString(), // Convert Date to ISO 8601 format
-        changeFrequency,
-    }));
+   
 
     const routes = ['', '/about', '/contact', '/blog'].map((route) => ({
         url: `${WEBSITE_HOST_URL}${route}`,
@@ -20,7 +16,6 @@ export default async function sitemap() {
     }));
 
     return [
-        ...routes,
-        ...posts
+        ...routes
     ];
 }
