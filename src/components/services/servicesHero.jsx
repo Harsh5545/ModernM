224c2031639f01e0790670a41a4dd6b16fe04f06
harshad <Harshadkajale333@gmail.com>
2024-06-14 12:45:46 +0530

    services page

---
+++ src/components/services/servicesHero.jsx
@@ -0,0 +1,22 @@
+import { BreadcrumbItem, Breadcrumbs } from '@nextui-org/react'
+import React from 'react'
+
+function ServicesHero() {
+  return (
+    <div className=' w-full bg-cover'>
+      <div className="bg-[url('/assets/ModernMannerismAbout.jpg')] bg-no-repeat bg-center w-full bg-cover min-h-[30vh] md:min-h-[40vh] flex items-center relative justify-center">
+        {/* Content goes here */}
+<div className='absolute inset-0 dark:bg-[#060507] bg-[#BEBEBE] dark:bg-opacity-60 bg-opacity-80'></div>
+        <div className="w-full px-16 relative z-50 dark:text-white text-black ">
+          <h1 className='p-1 font-bold text-5xl md:text-6xl'>Services</h1>
+          <Breadcrumbs className=" text-black p-4">
+            <BreadcrumbItem>Home</BreadcrumbItem>
+            <BreadcrumbItem>Services</BreadcrumbItem>
+          </Breadcrumbs>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ServicesHero
